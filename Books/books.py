05bffd28d7b996da1f5969c754cc7183dcde5a29
Matheus <dinizmaths@gmail.com>
2023-05-27 13:05:36 -0300

    include post and put cases

--- Books/books.py
+++ Books/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 BOOKS = [
@@ -21,28 +21,44 @@ async def home():
 async def read_all_books():
   return BOOKS
 
-@app.get("/books/?title={book_title}")
-async def read_book_by_title(book_title):
-  for book in BOOKS:
-    if book.get("title").casefold() == book_title.casefold():
-      return book
-
-@app.get("/books/?author={book_author}")
-async def read_book_by_author(book_author):
+@app.get("/books/{book_author}")
+async def read_books_by_author(book_author: str):
   books_by_author = []
 
   for book in BOOKS:
     if book.get("author").casefold() == book_author.casefold():
       books_by_author.append(book)
+    
+  return books_by_author if len(books_by_author) > 0 else "Not Found"
 
-  return books_by_author
-
-@app.get("/books/?category={book_category}")
-async def read_book_by_category(book_category):
+@app.get("/books/")
+async def read_book_by_category(book_category: str):
   books_by_category = []
 
   for book in BOOKS:
     if book.get("category").casefold() == book_category.casefold():
       books_by_category.append(book)
     
-  return books_by_category
+  return books_by_category if len(books_by_category) > 0 else "Not Found"
+
+@app.get("/books/{book_author}/")
+async def read_book_by_category(book_author: str, book_category: str):
+  books = []
+
+  for book in BOOKS:
+    if book.get("author").casefold() == book_author.casefold() and book.get("category").casefold() == book_category.casefold():
+      books.append(book)
+    
+  return books if len(books) > 0 else "Not Found"
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+  BOOKS.append(new_book)
+
+
+@app.put("/books/update_book/")
+async def update_book(updated_book=Body()):
+  for i in range(len(BOOKS)):
+    if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+      BOOKS[i] = updated_book
