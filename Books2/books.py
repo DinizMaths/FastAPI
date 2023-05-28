e9a4cd635409d2dbe944ea3b01b25de52cfcf40a
Matheus <dinizmaths@gmail.com>
2023-05-28 05:40:26 -0300

    adding path validation

--- Books2/books.py
+++ Books2/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 
 from typing import Optional
@@ -10,20 +10,23 @@ class Book:
   author: str
   description: str
   rating: int
+  published_date: int
 
-  def __init__(self, id, title, author, description, rating):
-    self.id = id
-    self.title = title
-    self.author = author
-    self.description = description
-    self.rating = rating
+  def __init__(self, id, title, author, description, rating, published_date):
+    self.id             = id
+    self.title          = title
+    self.author         = author
+    self.description    = description
+    self.rating         = rating
+    self.published_date = published_date
 
 class BookRequest(BaseModel):
-  id: Optional[int] = Field(title="id is not needed")
-  title: str        = Field(min_length=3)
-  author: str       = Field(min_length=2)
-  description: str  = Field(min_length=1, max_length=100)
-  rating: int       = Field(ge=0, le=5) # 0 <= x <= 5
+  id: Optional[int]   = Field(title="id is not needed")
+  title: str          = Field(min_length=3)
+  author: str         = Field(min_length=2)
+  description: str    = Field(min_length=1, max_length=100)
+  rating: int         = Field(ge=0, le=5) # 0 <= x <= 5
+  published_date: int = Field(ge=1900)
 
   class Config:
     schema_extra = {
@@ -31,18 +34,19 @@ class BookRequest(BaseModel):
         "title": "A new book",
         "author": "codingwithroby",
         "description": "A new description",
-        "rating": 5
+        "rating": 5,
+        "published_date": 2019
       }
     }
 
 
 BOOKS = [
-  Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
-  Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!',     5),
-  Book(3, 'Master Endpoints',     'codingwithroby', 'A awesome book!',   5),
-  Book(4, 'HP1',                  'Author 1',       'Book Description',  2),
-  Book(5, 'HP2',                  'Author 2',       'Book Description',  3),
-  Book(6, 'HP3',                  'Author 3',       'Book Description',  1)
+  Book(0, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
+  Book(1, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!',     5, 2030),
+  Book(2, 'Master Endpoints',     'codingwithroby', 'A awesome book!',   5, 2029),
+  Book(3, 'HP1',                  'Author 1',       'Book Description',  2, 2028),
+  Book(4, 'HP2',                  'Author 2',       'Book Description',  3, 2027),
+  Book(5, 'HP3',                  'Author 3',       'Book Description',  1, 2026)
 ]
 
 app = FastAPI()
@@ -57,7 +61,7 @@ async def read_all_books():
   return BOOKS
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(ge=0)):
   for book in BOOKS:
     if book.id == book_id:
       return book
@@ -68,6 +72,16 @@ async def read_book_by_rating(book_rating: int):
 
   for book in BOOKS:
     if book.rating == book_rating:
+      books.append(book)
+  
+  return books
+
+@app.get("/book/publish/")
+async def read_books_by_publish_date(published_date: int):
+  books = []
+
+  for book in BOOKS:
+    if book.published_date == published_date:
       books.append(book)
   
   return books
@@ -90,7 +104,7 @@ async def update_book(book: BookRequest):
       BOOKS[i] = book
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(ge=0)):
   for i in range(len(BOOKS)):
     if BOOKS[i].id == book_id:
       BOOKS.pop(i)
