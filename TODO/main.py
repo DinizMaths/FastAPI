0073607f614677e21f475cf984dccf42aa7773c1
Matheus <dinizmaths@gmail.com>
2023-05-28 21:32:01 -0300

    connecting

--- TODO/main.py
+++ TODO/main.py
@@ -1,9 +1,27 @@
 import models
 
-from fastapi  import FastAPI
-from database import engine
+from models import Todos
+
+from fastapi  import FastAPI, Depends
+from database import engine, SessionLocal
+
+from sqlalchemy.orm import Session
+
+from typing import Annotated
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+  db = SessionLocal()
+
+  try:
+    yield db
+  finally:
+    db.close()
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+  return db.query(Todos).all()
