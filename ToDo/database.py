5f19c817ba98056da68517e5a5d1c0ddee5d0ff1
Matheus <dinizmaths@gmail.com>
2023-06-06 18:23:35 -0300

    aaa

--- ToDo/database.py
+++ ToDo/database.py
@@ -3,10 +3,7 @@ from sqlalchemy.orm             import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-YOUR_POSTGRESQL_PASSWORD = ""
-YOUR_POSTGRESQL_SERVER   = ""
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{YOUR_POSTGRESQL_PASSWORD}@localhost/{YOUR_POSTGRESQL_SERVER}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:pgsqlPass@localhost/TodoApplicationDatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
