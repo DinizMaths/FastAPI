e5164592bc9a710d257ce22c0f93a69a74e57fc5
Matheus <dinizmaths@gmail.com>
2023-06-06 18:21:09 -0300

    send requirements

--- ToDo/database.py
+++ ToDo/database.py
@@ -2,6 +2,7 @@ from sqlalchemy                 import create_engine
 from sqlalchemy.orm             import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 YOUR_POSTGRESQL_PASSWORD = ""
 YOUR_POSTGRESQL_SERVER   = ""
 
